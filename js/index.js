582053887a0fa5ad7967c7ac5b85aaea3931fbb5
Max Franz <maxkfranz@gmail.com>
2016-12-07 18:20:35 -0500

    init commit

---
+++ js/index.js
@@ -0,0 +1,146 @@
+(function(){
+  document.addEventListener('DOMContentLoaded', function(){
+    let $$ = selector => Array.from( document.querySelectorAll( selector ) );
+    let $ = selector => document.querySelector( selector );
+
+    let tryPromise = fn => Promise.resolve().then( fn );
+
+    let toJson = obj => obj.json();
+    let toText = obj => obj.text();
+
+    let cy;
+
+    let $stylesheet = $('#style');
+    let getStylesheet = name => {
+      let convert = res => name.match(/[.]json$/) ? toJson(res) : toText(res);
+
+      return fetch(`stylesheets/${name}`).then( convert );
+    };
+    let applyStylesheet = stylesheet => {
+      if( typeof stylesheet === typeof '' ){
+        cy.style().fromString( stylesheet ).update();
+      } else {
+        cy.style().fromJson( stylesheet ).update();
+      }
+    };
+    let applyStylesheetFromSelect = () => Promise.resolve( $stylesheet.value ).then( getStylesheet ).then( applyStylesheet );
+
+    let $dataset = $('#data');
+    let getDataset = name => fetch(`datasets/${name}`).then( toJson );
+    let applyDataset = dataset => {
+      // so new eles are offscreen
+      cy.zoom(0.001);
+      cy.pan({ x: -9999999, y: -9999999 });
+
+      // replace eles
+      cy.elements().remove();
+      cy.add( dataset );
+    }
+    let applyDatasetFromSelect = () => Promise.resolve( $dataset.value ).then( getDataset ).then( applyDataset );
+
+    let calculateCachedCentrality = () => {
+      let nodes = cy.nodes();
+
+      if( nodes.length > 0 && nodes[0].data('centrality') == null ){
+        let centrality = cy.elements().closenessCentralityNormalized();
+
+        nodes.forEach( n => n.data( 'centrality', centrality.closeness(n) ) );
+      }
+    };
+
+    let $layout = $('#layout');
+    let maxLayoutDuration = 1500;
+    let layoutPadding = 50;
+    let concentric = function( node ){
+      calculateCachedCentrality();
+
+      return node.data('centrality');
+    };
+    let levelWidth = function( nodes ){
+      calculateCachedCentrality();
+
+      let min = nodes.min( n => n.data('centrality') ).value;
+      let max = nodes.max( n => n.data('centrality') ).value;
+
+
+      return ( max - min ) / 5;
+    };
+    let layouts = {
+      cola: {
+        name: 'cola',
+        padding: layoutPadding,
+        nodeSpacing: 12,
+        edgeLengthVal: 45,
+        animate: true,
+        randomize: true,
+        maxSimulationTime: maxLayoutDuration,
+        boundingBox: { // to give cola more space to resolve initial overlaps
+          x1: 0,
+          y1: 0,
+          x2: 10000,
+          y2: 10000
+        },
+        edgeLength: function( e ){
+          let w = e.data('weight');
+
+          if( w == null ){
+            w = 0.5;
+          }
+
+          return 45 / w;
+        }
+      },
+      concentricCentrality: {
+        name: 'concentric',
+        padding: layoutPadding,
+        animate: true,
+        animationDuration: maxLayoutDuration,
+        concentric: concentric,
+        levelWidth: levelWidth
+      },
+      concentricHierarchyCentrality: {
+        name: 'concentric',
+        padding: layoutPadding,
+        animate: true,
+        animationDuration: maxLayoutDuration,
+        concentric: concentric,
+        levelWidth: levelWidth,
+        sweep: Math.PI * 2 / 3,
+        clockwise: true,
+        startAngle: Math.PI * 1 / 6
+      },
+      custom: { // replace with your own layout parameters
+        name: 'preset',
+        padding: layoutPadding
+      }
+    };
+    let prevLayout;
+    let getLayout = name => Promise.resolve( layouts[ name ] );
+    let applyLayout = layout => {
+      if( prevLayout ){
+        prevLayout.stop();
+      }
+
+      let l = prevLayout = cy.makeLayout( layout );
+
+      return l.run().promiseOn('layoutstop');
+    }
+    let applyLayoutFromSelect = () => Promise.resolve( $layout.value ).then( getLayout ).then( applyLayout );
+
+    cy = window.cy = cytoscape({
+      container: $('#cy')
+    });
+
+    tryPromise( applyDatasetFromSelect ).then( applyStylesheetFromSelect ).then( applyLayoutFromSelect );
+
+    $dataset.addEventListener('change', function(){
+      tryPromise( applyDatasetFromSelect ).then( applyLayoutFromSelect );
+    });
+
+    $stylesheet.addEventListener('change', applyStylesheetFromSelect);
+
+    $layout.addEventListener('change', applyLayoutFromSelect);
+
+    $('#redo-layout').addEventListener('click', applyLayoutFromSelect);
+  });
+})();
